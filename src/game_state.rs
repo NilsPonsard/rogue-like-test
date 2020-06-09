7e4519993cf4b4dcbe5106ec6753b26ad61329f6
Nils Ponsard <nilsponsard@gmail.com>
2020-06-09 18:54:49 +0200

    ggez

---
+++ src/game_state.rs
@@ -0,0 +1,128 @@
+use ggez::event::{self, EventHandler};
+use ggez::event::{KeyCode, KeyMods};
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use std::time::{Duration, Instant};
+
+pub const UPDATES_PER_SECOND: f32 = 60.0;
+pub const MILLIS_PER_UPDATE: u64 = (1.0 / UPDATES_PER_SECOND * 1000.0) as u64;
+
+struct Assets {
+    player_image: graphics::Image,
+    // shot_image: graphics::Image,
+    // rock_image: graphics::Image,
+}
+
+impl Assets {
+    fn new(ctx: &mut Context) -> GameResult<Assets> {
+        let player_image = graphics::Image::new(ctx, "/assets/player/placeholder.png")?;
+        // let shot_image = graphics::Image::new(ctx, "/shot.png")?;
+        // let rock_image = graphics::Image::new(ctx, "/rock.png")?;
+
+        Ok(Assets {
+            player_image,
+            // shot_image,
+            // rock_image
+        })
+    }
+}
+
+struct Direction {
+    right: bool,
+    left: bool,
+    up: bool,
+    down: bool,
+}
+impl Direction {
+    pub fn new() -> Direction {
+        Direction {
+            right: false,
+            left: false,
+            up: false,
+            down: false,
+        }
+    }
+}
+
+pub struct MainState {
+    x: f32,
+    y: f32,
+    dir: Direction,
+    last_update: Instant,
+    assets: Assets,
+}
+
+impl MainState {
+    pub fn new(ctx: &mut Context) -> GameResult<MainState> {
+        println!("Game resource path: {:?}", ctx.filesystem);
+        let assets = Assets::new(ctx)?;
+
+        let s = MainState {
+            x: 0.0,
+            y: 0.0,
+            dir: Direction::new(),
+            last_update: Instant::now(),
+            assets,
+        };
+        Ok(s)
+    }
+}
+
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        if Instant::now() - self.last_update >= Duration::from_millis(MILLIS_PER_UPDATE) {
+            if self.dir.up {
+                self.y -= 2.0
+            }
+            if self.dir.down {
+                self.y += 2.0
+            }
+            if self.dir.right {
+                self.x += 2.0
+            }
+            if self.dir.left {
+                self.x -= 2.0
+            }
+        }
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+        {
+            let drawparams =
+                graphics::DrawParam::new().dest(ggez::nalgebra::Point2::new(self.x, self.y));
+            graphics::draw(ctx, &self.assets.player_image, drawparams)?;
+        }
+        graphics::present(ctx)
+    }
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: KeyCode,
+        _keymod: KeyMods,
+        repeat: bool,
+    ) {
+        // println!("{}", repeat);
+        if !repeat {
+            // do only once by keypress
+            match keycode {
+                KeyCode::Left => self.dir.left = true,
+                KeyCode::Right => self.dir.right = true,
+                KeyCode::Up => self.dir.up = true,
+                KeyCode::Down => self.dir.down = true,
+
+                _ => {}
+            }
+        }
+    }
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods) {
+        match keycode {
+            KeyCode::Left => self.dir.left = false,
+            KeyCode::Right => self.dir.right = false,
+            KeyCode::Up => self.dir.up = false,
+            KeyCode::Down => self.dir.down = false,
+
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,25 @@
-use amethyst::{
-    core::transform::TransformBundle,
-    prelude::*,
-    renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
-    },
-    utils::application_root_dir,
-};
+use ggez::event;
+use ggez::{ContextBuilder, GameResult};
 
-struct MyState;
+mod game_state;
 
-impl SimpleState for MyState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
-}
+fn main() -> GameResult {
+    let resource_dir = if let Ok(manifest_dir) = std::env::var("CARGO_MANIFEST_DIR") {
+        let mut path = std::path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        std::path::PathBuf::from("./resources")
+    };
+    // Make a Context.
+    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .add_resource_path(resource_dir)
+        .build()
+        .expect("aieee, could not create ggez context!");
 
-fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
-
-    let app_root = application_root_dir()?;
-
-    let assets_dir = app_root.join("assets");
-    let config_dir = app_root.join("config");
-    let display_config_path = config_dir.join("display.ron");
-
-    let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
-                )
-                .with_plugin(RenderFlat2D::default()),
-        )?
-        .with_bundle(TransformBundle::new())?;
-
-    let mut game = Application::new(assets_dir, MyState, game_data)?;
-    game.run();
-
-    Ok(())
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut game = game_state::MainState::new(&mut ctx)?;
+    event::run(&mut ctx, &mut event_loop, &mut game)
 }
